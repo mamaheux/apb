89003a1246753b1e4ea9ebfa344e5edab3f36f76
Marc-Antoine Maheux <mahm1904@usherbrooke.ca>
2018-07-24 21:30:38 -0400

    Add loop unrolling macro

---
+++ source/Utils/include/Optimization/LoopUnrolling.h
@@ -0,0 +1,115 @@
+#ifndef LOOP_UNROLLING_H
+#define LOOP_UNROLLING_H
+
+#define PRIVATE_DUPLICATE_CODE_X1(code)  {code}
+#define PRIVATE_DUPLICATE_CODE_X2(code)  {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X3(code)  {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X4(code)  {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X5(code)  {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X6(code)  {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X7(code)  {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X8(code)  {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X9(code)  {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X11(code) PRIVATE_DUPLICATE_CODE_X10(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X12(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X13(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X14(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X15(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X16(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X17(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X18(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X19(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X20(code) PRIVATE_DUPLICATE_CODE_X10(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X21(code) PRIVATE_DUPLICATE_CODE_X20(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X22(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X23(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X24(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X25(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X26(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X27(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X28(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X29(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X30(code) PRIVATE_DUPLICATE_CODE_X20(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X31(code) PRIVATE_DUPLICATE_CODE_X30(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X32(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X33(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X34(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X35(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X36(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X37(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X38(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X39(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X40(code) PRIVATE_DUPLICATE_CODE_X30(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X41(code) PRIVATE_DUPLICATE_CODE_X40(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X42(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X43(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X44(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X45(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X46(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X47(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X48(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X49(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X50(code) PRIVATE_DUPLICATE_CODE_X40(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X51(code) PRIVATE_DUPLICATE_CODE_X50(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X52(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X53(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X54(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X55(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X56(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X57(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X58(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X59(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X60(code) PRIVATE_DUPLICATE_CODE_X50(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X61(code) PRIVATE_DUPLICATE_CODE_X60(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X62(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X63(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X64(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X65(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X66(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X67(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X68(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X69(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X70(code) PRIVATE_DUPLICATE_CODE_X60(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X71(code) PRIVATE_DUPLICATE_CODE_X70(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X72(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X73(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X74(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X75(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X76(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X77(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X78(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X79(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X80(code) PRIVATE_DUPLICATE_CODE_X70(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X81(code) PRIVATE_DUPLICATE_CODE_X80(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X82(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X83(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X84(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X85(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X86(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X87(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X88(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X89(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X90(code) PRIVATE_DUPLICATE_CODE_X80(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define PRIVATE_DUPLICATE_CODE_X91(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code}
+#define PRIVATE_DUPLICATE_CODE_X92(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X93(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X94(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X95(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X96(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X97(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X98(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X99(code)  PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code} {code} {code} {code} {code} {code}
+#define PRIVATE_DUPLICATE_CODE_X100(code) PRIVATE_DUPLICATE_CODE_X90(code) {code} {code} {code} {code} {code} {code} {code} {code} {code} {code}
+
+#define LOOP_UNROLLING(count, code) do{ PRIVATE_DUPLICATE_CODE_X##count(code) } while(false)
+#endif
